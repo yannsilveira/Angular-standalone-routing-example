e66f5ad80012a0c667b9e4b0f2c29569c35842d8
Yann Ap. da Silveira <56853103+yannsilveira@users.noreply.github.com>
2024-06-06 22:56:47 -0300

    Project to explain how to use standalone components on routing files.

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  templateUrl: './dashboard.component.html',
+  standalone: true,
+  imports: [RouterModule]
+})
+export class DashboardComponent {}
